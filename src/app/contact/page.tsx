fe37c3087fc9113401de3d979dd61ad9a62bd01d
Mohamed Larabi <molarabi007@gmail.com>
2025-11-01 14:41:25 +0000

    ajoute l'adresse avec une carte  (_for element div_)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -46,6 +46,20 @@ export default function ContactPage() {
             ))}
           </div>
         </div>
+        
+        <div className="mt-20 md:mt-24">
+            <div className="w-full h-[400px] rounded-lg overflow-hidden shadow-xl">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.2929737877233!2d-122.41941548468155!3d37.77492957975871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858064a8525b39%3A0x7c81ef1b0c036a4!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1628000000000!5m2!1sen!2sus"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
+                    allowFullScreen={true}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
+        </div>
       </div>
     </>
   );
