ce62a1a7a7ff642d6ce6b4577a6242c69f1ce4e4
Mohamed Larabi <molarabi007@gmail.com>
2025-11-05 18:33:38 +0000

    non dezzome aussi sans laisser des vide (_for element div_)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
               alt={heroImage.description}
               data-ai-hint={heroImage.imageHint}
               fill
-              className="object-cover object-[center_top]"
+              className="object-cover object-center"
               priority
             />
           )
