51ac88ce26bf2d0b27c4ee54af7fcfeeeefc9b27
Mohamed Larabi <molarabi007@gmail.com>
2025-11-05 18:23:02 +0000

    l'image hero est trop zoudme tu la dezoume un peu (_for element div_)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
               alt={heroImage.description}
               data-ai-hint={heroImage.imageHint}
               fill
-              className="object-cover"
+              className="object-contain"
               priority
             />
           )
